9efc49f145d448d9bff5c1ede83817e7ec5f9c3e
brandonj987 <98341991+brandonj987@users.noreply.github.com>
2022-01-31 19:39:39 +0000

    Update main.java
    Examples using the classes and methods

--- src/main/java/com/spartaglobal/algorithms/main.java
+++ src/main/java/com/spartaglobal/algorithms/main.java
@@ -4,8 +4,28 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        int[] ints = {5,654,4,6,45,64,56,45,56,4,65,65,54,897,69,56,496,46,56,6,986,4,6,9,6,6,654,96,49,84,964,6,49,4,64,64,964,964535,35496,4};
+        // Call sortArray and pass in an array of ints to be sorted or a size and number boundary for a random array
+        // Uses both the bubble sort and merge sort and outputs their results as a string
+        int[] ints = {5,54,8574,854,854,2,45,4,54,85,487,58,5845,548};
+        int[] ints2 = ints.clone();
+        SortArray s = new SortArray();
+        System.out.println(s.sortArray(50, 1000));
+        System.out.println(s.sortArray(ints));
+
+        // Call merge or bubble sort individually to sort an array of ints or random array of specified size and number boundary
+        // returns the sorted array as an int[]
+        MergeSort m = new MergeSort();
+        m.mergeSort(ints);
+        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(m.mergeSort(40, 60)));
+
         BubbleSort b = new BubbleSort();
-        System.out.println(Arrays.toString(b.bubbleSort(ints)));
+        b.bubbleSort(ints2);
+        System.out.println(Arrays.toString(ints2));
+        System.out.println(Arrays.toString(b.bubbleSort(40, 60)));
+
+
+        RandomArray r = new RandomArray();
+        System.out.println(Arrays.toString(r.randArray(50,100)));
     }
 }
